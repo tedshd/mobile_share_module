3c84dfe0462f6fc88cdd0d4563b8f7f620fb2177
tedshd <tedshd@gmail.com>
2018-02-26 14:31:43 +0800

    [dev] Modify code.

--- js/mobile_share_module.js
+++ js/mobile_share_module.js
@@ -10,34 +10,45 @@ function mobile_share_module(url, option) {
         console.error('url is require');
         return;
     }
-    var app_id = (option && option.fbAppId) ? '&app_id=' + option.fbAppId : '',
+    console.log(option);
+    var iconLine = (option && option.iconLine) ? option.iconLine : '',
+        iconLine2x = (option && option.iconLine2x) ? option.iconLine2x : '',
+        iconFacebook = (option && option.iconFacebook) ? option.iconFacebook : '',
+        iconFacebook2x = (option && option.iconFacebook2x) ? option.iconFacebook2x : '',
+        iconMessenger = (option && option.iconMessenger) ? option.iconMessenger : '',
+        iconMessenger2x = (option && option.iconMessenger2x) ? option.iconMessenger2x : '',
+        iconGoogle = (option && option.iconGoogle) ? option.iconGoogle : '',
+        iconGoogle2x = (option && option.iconGoogle2x) ? option.iconGoogle2x : '',
+        iconTwitter = (option && option.iconTwitter) ? option.iconTwitter : '',
+        iconTwitter2x = (option && option.iconTwitter2x) ? option.iconTwitter2x : '',
+        app_id = (option && option.fbAppId) ? '&app_id=' + option.fbAppId : '',
         text = (option && option.twitterText) ? '&text=' + option.twitterText : '',
         ref = (option && option.twitterRef) ? '&original_referer=' + option.twitterRef : '',
         obj = {
         'line': {
             'href': 'https://line.naver.jp/R/msg/text/?' + encodeURIComponent(url),
-            'icon': './img/icon_line_32.png',
-            'icon_2x': './img/icon_line_64.png',
+            'icon': iconLine,
+            'icon_2x': iconLine2x,
         },
         'facebook': {
             'href': 'https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(url),
-            'icon': './img/icon_fb_32.png',
-            'icon_2x': './img/icon_fb_64.png',
+            'icon': iconFacebook,
+            'icon_2x': iconFacebook2x,
         },
         'messenger': {
             'href': 'fb-messenger://share/?link=' + encodeURIComponent(url) + app_id,
-            'icon': './img/icon_messenger_32.png',
-            'icon_2x': './img/icon_messenger_64.png',
+            'icon': iconMessenger,
+            'icon_2x': iconMessenger2x,
         },
         'google': {
             'href': 'https://plus.google.com/share?url=' + encodeURIComponent(url),
-            'icon': './img/icon_google_32.png',
-            'icon_2x': './img/icon_google_64.png',
+            'icon': iconGoogle,
+            'icon_2x': iconGoogle2x,
         },
         'twitter': {
             'href': 'https://twitter.com/intent/tweet?url=' + encodeURIComponent(url) + text + ref,
-            'icon': './img/icon_twitter_32.png',
-            'icon_2x': './img/icon_twitter_64.png',
+            'icon': iconTwitter,
+            'icon_2x': iconTwitter2x,
         }
     };
 
